efa3c0a3d1db891e077aa089bd9ee900427b501b
danimij <danimij@hotmail.com>
2026-07-02 14:42:55 +0200

    Fix base path to match GitHub repo name FaceO

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/faceo/',
+  base: '/FaceO/',
   server: {
     port: parseInt(process.env.PORT || '5174'),
   },
